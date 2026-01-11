969c46509a7968bd703c2f02495e5a5b1ce855c6
ttop32 <ttop324@gmail.com>
2026-01-11 23:15:43 +0900

    test

--- Auto Screen Brightness/TrayIconManager.cs
+++ Auto Screen Brightness/TrayIconManager.cs
@@ -3,41 +3,48 @@ using System.Runtime.InteropServices;
 using Microsoft.UI.Xaml;
 using WinRT.Interop;
 
-namespace Auto_Screen_Brightness
-{
-    public static class TrayIconManager
-    {
+namespace Auto_Screen_Brightness {
+    public static class TrayIconManager {
         private static IntPtr _hwnd;
         private static NotifyIconWrapper? _icon;
 
-        public static void Initialize(Window window)
-        {
+        [DllImport("user32.dll")]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+        private const int SW_RESTORE = 9;
+
+        // 초기화
+        public static void Initialize(Window window) {
             _hwnd = WindowNative.GetWindowHandle(window);
             _icon = new NotifyIconWrapper();
             _icon.Show();
-            _icon.OnLeftClick += () =>
-            {
-                window.DispatcherQueue.TryEnqueue(() =>
-                {
-                    window.Activate();
+
+            _icon.OnLeftClick += () => {
+                window.DispatcherQueue.TryEnqueue(() => {
+                    ShowWindow(window);
                 });
             };
-            _icon.OnExit += () =>
-            {
-                _icon.Dispose();
-                Environment.Exit(0);
-            };
         }
 
-        public static void HideWindow(Window window)
-        {
-            NativeMethods.ShowWindow(_hwnd, 0); // SW_HIDE
+        // 기존 코드와 호환
+        public static void HideWindow(Window window) {
+            ShowWindow(_hwnd, SW_HIDE);
         }
 
-        public static void ShowWindow(Window window)
-        {
-            NativeMethods.ShowWindow(_hwnd, 5); // SW_SHOW
+        public static void ShowWindow(Window window) {
+            ShowWindow(_hwnd, SW_RESTORE);
+            ShowWindow(_hwnd, SW_SHOW);
+            SetForegroundWindow(_hwnd);
             window.Activate();
         }
+
     }
 }
